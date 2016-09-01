3f3b75f074ba57f6ed7d031f483fd51d4a9c3381
Kudryaev Grigoriy <gkudryaev@gmail.com>
2016-09-01 13:40:42 +0300

    Initial Commit

---
+++ iPromatSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPromatSwift
+//
+//  Created by Kudryaev, Grigoriy on 01.09.16.
+//  Copyright © 2016 Kudryaev, Grigoriy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
